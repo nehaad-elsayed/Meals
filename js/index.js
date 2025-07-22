20ca8c13cfbc6be259458c8d0630071b66818795
Nehad <nehad.elsayed@robustastudio.com>
2025-07-23 02:49:07 +0300

    build: setup Parcel, update project structure, and add .gitignore

--- js/index.js
+++ js/index.js
@@ -1,17 +1,23 @@
-//&&&&&&&&&&&&&& Globalllll
-
+//&&&&&&&&&&&&&& Global
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../css/master.css";
+import "toastr/build/toastr.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import toastr from "toastr";
 const rowData = document.getElementById("rowData");
 const searchInput = document.getElementById("searchInput");
 const searchBtn = document.getElementById("searchBtn");
 const loading = document.querySelector(".loading");
-const api_URL = "https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken";
+// const api_URL = "https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken";
+// const api_ForMealDetails = "https://www.themealdb.com/api/json/v1/1/lookup.php?i='1537'"
 let meals = [];
 
 
 getMeals();
 
 //&&&&&&&&& eventssssss
-//reltime search
+//Realtime search
 // searchInput.addEventListener("input",()=>{
 //  getMeals(searchInput.value);
 // })
@@ -36,7 +42,7 @@ async function getMeals(meal = "chicken") {
     meals = data.meals;
     displayMeals(meals);
   } catch (error) {
-    toastr.error("invalid ingradient");
+    toastr.error(`invalid ingradient <i class="fa-solid fa-face-tired"></i>`);
   } finally {
     loading.classList.add("d-none");
   }
@@ -45,34 +51,70 @@ async function getMeals(meal = "chicken") {
 
 
 
+// function displayMeals(arr) {
+//   let box = "";
+
+//   for (let i = 0; i < arr.length; i++) {
+//     box += `
+
+//  <div class="col-md-3">
+//                         <div class="card h-100">
+//                             <img height="200px" class="card-img-top w-100 d-block" src="${
+//                               arr[i].strMealThumb
+//                             }" alt=""/>
+//                             <div class="card-body text-center">
+//                                 <h2 class="card-title">${arr[i].strMeal
+//                                   .split(" ", 2)
+//                                   .join(" ")}</h2>
+//                                 <button data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="showDetails(${ arr[i].idMeal})" class="w-100 btn btn-outline-success mt-2">Show details</button>
+//                             </div>
+//                         </div>
+//                     </div>
+
+// `;
+//   }
+//   rowData.innerHTML = box;
+// }
+
+
+
+
+//&& 3shan bst5dm bundler elprev solution XXX 
+
+
+
 function displayMeals(arr) {
   let box = "";
 
   for (let i = 0; i < arr.length; i++) {
     box += `
-
- <div class="col-md-3">
-                        <div class="card h-100">
-                            <img height="200px" class="card-img-top w-100 d-block" src="${
-                              arr[i].strMealThumb
-                            }" alt=""/>
-                            <div class="card-body text-center">
-                                <h2 class="card-title">${arr[i].strMeal
-                                  .split(" ", 2)
-                                  .join(" ")}</h2>
-                                <button data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="showDetails(${
-                                  arr[i].idMeal
-                                })" class="w-100 btn btn-outline-success mt-2">Show details</button>
-                            </div>
-                        </div>
-                    </div>
-
-`;
+      <div class=" col-12 col-md-6 col-lg-3">
+        <div class="card h-100">
+          <img height="200px" class="card-img-top w-100 d-block" src="${arr[i].strMealThumb}" alt=""/>
+          <div class="card-body text-center">
+            <h2 class="card-title">${arr[i].strMeal.split(" ", 2).join(" ")}</h2>
+            <button 
+              data-bs-toggle="modal" 
+              data-bs-target="#exampleModal" 
+              data-id="${arr[i].idMeal}"
+              class="w-100 btn btn-outline-success mt-2 details-btn">
+              Show details
+            </button>
+          </div>
+        </div>
+      </div>
+    `;
   }
+
   rowData.innerHTML = box;
+
+  document.querySelectorAll(".details-btn").forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const id = btn.getAttribute("data-id");
+      showDetails(id);
+    });
+  });
 }
-
-
 
 
 
@@ -81,9 +123,9 @@ async function showDetails(id) {
     `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
   );
   const data = await response.json();
-  console.log(data.meals[0]);
+  console.log(data.meals[0]); //3shan elresponse gay array feeha one obj
   const modalBody = document.getElementById("modalBodyData");
-  modalBody.innerHTML = "";
+  modalBody.innerHTML = ""; // 3shan afdy elbody f kol marra abl elnew meal
   let img = document.createElement("img");
   let h3 = document.createElement("h3");
   let ancor = document.createElement("a");
